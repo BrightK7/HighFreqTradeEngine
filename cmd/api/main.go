445886c5d7376e76d48a39d3a914a38111ff6e8b
BrightK7 <32251602+BrightK7@users.noreply.github.com>
2024-10-30 05:39:12 +0000

    feat: update READ file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -28,7 +28,7 @@ type application struct {
 
 func main() {
 	var cfg config
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.IntVar(&cfg.port, "port", 8080, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
